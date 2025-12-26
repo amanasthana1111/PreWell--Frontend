1b4b1f77a9432c311aedb37a7ed9a5d16dd9f7be
amanasthana1111 <as.asthana.1111@gmail.com>
2025-12-26 12:18:09 +0530

    fix usereffect error

--- src/components/BeforeAuth/Login.jsx
+++ src/components/BeforeAuth/Login.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Loading from "./Loading";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
