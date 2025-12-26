7e62bb0f045c2111439864069fe7eb71d0665134
amanasthana1111 <as.asthana.1111@gmail.com>
2025-12-26 14:04:26 +0530

    add my resumw

--- src/components/BeforeAuth/MyAccount.jsx
+++ src/components/BeforeAuth/MyAccount.jsx
@@ -13,13 +13,18 @@ function MyAccount() {
 
   if (!isAuth) return null;
 
-  const handleResumeUpload = (e) => {
+  const handleResumeUpload = async (e) => {
     setResume(e.target.files[0]);
+    try {
+      const response = await axios.post("prewell-backend-2.onrender.com/resumes/upload",resume,{
+          withCredentials: true,
+        }
+    )
+    } catch (error) {
+      
+    }
   };
 
-  const handlePasswordChange = () => {
-    alert("Redirect to change password page");
-  };
 
   return (
     <div className="max-w-3xl mx-auto p-6">
@@ -32,16 +37,6 @@ function MyAccount() {
         <p><strong>Email:</strong> {user?.email || "N/A"}</p>
       </div>
 
-      {/* Change Password */}
-      <div className="bg-white shadow rounded-lg p-4 mb-6">
-        <h2 className="text-lg font-semibold mb-3">Security</h2>
-        <button
-          onClick={handlePasswordChange}
-          className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
-        >
-          Change Password
-        </button>
-      </div>
 
       {/* Resume Upload */}
       <div className="bg-white shadow rounded-lg p-4 mb-6">
