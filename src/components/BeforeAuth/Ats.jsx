7cd091794189be482da5c57975c07c4972a52acb
amanasthana1111 <as.asthana.1111@gmail.com>
2025-12-27 17:15:22 +0530

    add some ats logic testing

--- src/components/BeforeAuth/Ats.jsx
+++ src/components/BeforeAuth/Ats.jsx
@@ -1,16 +1,39 @@
+import { useEffect, useState } from "react";
 import useUserAuth from "./hooks/useAuth";
+import axios from "axios";
 
 
 const Ats = () => {
   const isAuth = useUserAuth();
+  const [isResumeUploaded , setResumeUploaded] = useState(null)
+  const [Ats , setAts] = useState(null)
+
+  useEffect(()=>{
+    const check = async()=>{
+      try {
+        const response = await axios.get("https://prewell-backend-2.onrender.com/start/atsScanner",{
+        withCredentials : true
+      })
+      setAts(response.data);
+      setResumeUploaded(true)
+      
+      } catch (error) {
+        setResumeUploaded(false)
+      }
+    }
+  },[])
 
   if (isAuth === null) {
     return <div>Loading...</div>;
   }
 
   if (!isAuth) return null;
+  
 
-  return <div>Ats</div>;
+  return <div>
+    {isResumeUploaded && <h1>OK</h1>}
+    
+  </div>;
 };
 
 export default Ats;
